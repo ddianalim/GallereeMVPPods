d1b637816110bdc104b8a5e2fc25648e1dbbcc1e
dianalimnos <dianalim1117@gmail.com>
2016-07-20 13:57:50 -0700

    Added Pods file to Nat version

---
+++ Galleree/HomeView.swift
@@ -0,0 +1,16 @@
+//
+//  HomeView.swift
+//  Galleree
+//
+//  Created by Natalie Lim on 7/18/16.
+//  Copyright © 2016 Natalie Lim. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController : UIViewController {
+    
+    @IBAction func unwindToListNotesViewController(segue: UIStoryboardSegue) {
+    }
+    
+}
---
+++ Galleree/UserView.swift
@@ -0,0 +1,13 @@
+//
+//  UserView.swift
+//  Galleree
+//
+//  Created by Natalie Lim on 7/18/16.
+//  Copyright © 2016 Natalie Lim. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+}
